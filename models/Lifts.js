e5d04869b8027a882d79a8a0e50ee47691b45228
ShaunApps <shaun.apps@gmail.com>
2017-01-02 19:39:18 -0500

    further minor changes

--- models/Lifts.js
+++ models/Lifts.js
@@ -1,6 +1,9 @@
 var db = require('./index.js');
+var Sequelize = require('sequelize');
 
-var Lift = db.sequelize.define('lift', {
+var sequelize = db.sequelize;
+
+var Lift = sequelize.define('lift', {
   name: {
     type: Sequelize.STRING,
     field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
@@ -14,10 +17,15 @@ var Lift = db.sequelize.define('lift', {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
-Lift.sync({force: true}).then(function () {
+sequelize
+  .sync({
+    force: true
+  })
+  .then(function() {
   // Table created
-  return Lift.create({
-    name: 'Chest Press',
-    muscleGroup: 'Chest'
-  });
+    var liftInstance = Lift.build({
+      name: 'Chest Press',
+      muscleGroup: 'Chest'
+    })
+    liftInstance.save();
 });
