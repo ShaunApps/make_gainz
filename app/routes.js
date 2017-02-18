f7766091f7da67bfada0e0a87916fa7897189bd2
ShaunApps <shaun.apps@gmail.com>
2017-02-17 23:05:20 -0500

    need work on api routes to make searchable muscle groups

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const morgan = require('morgan'); // logger for API
 const exercises = require('./routes/exercises'); //routes are defined here
 const app = express(); //Create the Express app
 
